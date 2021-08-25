eafa210608df979743bd140e4a3a03bed11c542e
Leo Carvalho <carvalho.csleo@gmail.com>
2021-08-24 23:11:29 -0300

    Create a function to handle with analytics

---
+++ src/services/analytics/index.js
@@ -0,0 +1,14 @@
+import useAckee from "use-ackee";
+function Analytics() {
+  const history = `${document.location.pathname}${document.location.hash}`
+  useAckee(history, {
+    domainId: '8cf0d117-2986-4322-8552-b001ee61b3d0',
+    server: 'https://analytics.leocarvalho.dev'
+  }, {
+    detailed: true,
+    ignoreLocalhost: true,
+    ignoreOwnVisits: true
+  });
+}
+
+export default Analytics;
