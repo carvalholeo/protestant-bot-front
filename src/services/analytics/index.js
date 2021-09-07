7f1d65696cc9ece188e20faa208e254ac2dcdf11
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-07 02:08:12 -0300

    Add semicolon at end of the line

--- src/services/analytics/index.js
+++ src/services/analytics/index.js
@@ -1,6 +1,6 @@
 import useAckee from "use-ackee";
 function Analytics() {
-  const history = `${document.location.pathname}${document.location.hash}`
+  const history = `${document.location.pathname}${document.location.hash}`;
   useAckee(history, {
     domainId: '8cf0d117-2986-4322-8552-b001ee61b3d0',
     server: 'https://analytics.leocarvalho.dev'
