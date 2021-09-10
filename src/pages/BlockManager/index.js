8baafcd07cb07987ac4271e0e954dcf15595854b
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-09 23:12:14 -0300

    Change Window native alert to Bootstrap version

--- src/pages/BlockManager/index.js
+++ src/pages/BlockManager/index.js
@@ -1,4 +1,5 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+import Toast from "bootstrap/js/src/toast";
 
 import apiProtestantBot from '../../services/apiProtestantBot';
 import analytics from '../../services/analytics';
@@ -9,6 +10,19 @@ function BlockManager() {
   analytics();
   const [userBlock, setUserBlock] = useState('');
   const [userUnblock, setUserUnblock] = useState('');
+  const [apiResponse, setApiResponse] = useState({
+    title: "Bloqueio e desbloqueio de usuário",
+    time: "Alguns segundos atrás",
+    className: "warning text-dark",
+    message: 'Você precisa preencher um usuário para ser bloqueado ou desbloqueado.'
+  });
+  const [toast, setToast] = useState(new Toast());
+
+  useEffect(() => {
+    const toastLive = document.getElementById("liveToast");
+    const toast = new Toast(toastLive);
+    setToast(toast);
+  }, []);
 
   function handleUser(event, block = true) {
     const inputDirty = event.target.value;
@@ -24,8 +38,8 @@ function BlockManager() {
   async function handleBlock(event) {
     event.preventDefault();
 
-    if(userBlock === '') {
-      window.alert('Você precisa preencher um usuário para ser bloqueado.');
+    if (userBlock === '') {
+      toast.show();
       return;
     }
 
@@ -35,15 +49,26 @@ function BlockManager() {
       user: userBlock
     };
 
-    if(confirmation) {
+    if (confirmation) {
       await apiProtestantBot.post('/blocklist/block', data)
-          .then(() => {
-            alert(`Usuário @${userBlock} bloqueado com sucesso.`);
-            setUserBlock('');
-          })
-          .catch(() => {
-            alert('Tivemos um problema para bloquear. Por favor, tente novamente mais tarde.');
+        .then(() => {
+          setApiResponse({
+            title: "Desbloquear usuário",
+            time: "Alguns segundos atrás",
+            className: "success",
+            message: `Usuário @${userBlock} bloqueado com sucesso.`
           });
+          setUserBlock('');
+        })
+        .catch(error => {
+          setApiResponse({
+            title: "Desbloquear usuário",
+            time: "Alguns segundos atrás",
+            className: "danger",
+            message: `Tivemos um problema para te bloquear e tudo que este app sabe é: "${error.message}". Tente de novo mais tarde.`,
+          });
+        })
+        .finally(() => toast.show());
     } else {
       alert('Bloqueio cancelado');
     }
@@ -52,8 +77,8 @@ function BlockManager() {
   async function handleUnblock(event) {
     event.preventDefault();
 
-    if(userUnblock === '') {
-      window.alert('Você precisa preencher um usuário para ser desbloqueado.');
+    if (userUnblock === '') {
+      toast.show();
       return;
     }
 
@@ -62,68 +87,102 @@ function BlockManager() {
     };
 
     await apiProtestantBot.put('/blocklist/unblock', data)
-        .then(() => {
-          alert(`Usuário @${userUnblock} foi desbloqueado com sucesso. Seja bem-vindx de volta!`);
-          setUserUnblock('');
-        })
-        .catch(() => {
-          alert('Tivemos um problema para te desbloquear. Tente de novo mais tarde.');
+      .then(() => {
+        setApiResponse({
+          title: "Desbloquear usuário",
+          time: "Alguns segundos atrás",
+          className: "success",
+          message: `Usuário @${userUnblock} foi desbloqueado com sucesso. Seja bem-vindx de volta!`,
         });
+        setUserUnblock('');
+      })
+      .catch((error) => {
+        setApiResponse({
+          title: "Desbloquear usuário",
+          time: "Alguns segundos atrás",
+          className: "danger",
+          message: `Tivemos um problema para te desbloquear e tudo que este app sabe é: "${error.message}". Tente de novo mais tarde.`,
+        });
+      })
+      .finally(() => toast.show());
   }
 
 
   return (
     <main>
-    <div className='container'>
-      <Breadcrumb message="Bloquear o bot" />
+      <div className='container'>
+        <Breadcrumb message="Bloquear o bot" />
 
-      <h1>Bloqueio e desbloqueio do bot</h1>
-      <p>Esta área é para você fazer o bloqueio ou o desbloqueio de um usuário.</p>
+        <h1>Bloqueio e desbloqueio do bot</h1>
+        <p>Esta área é para você fazer o bloqueio ou o desbloqueio de um usuário.</p>
 
-      <h2>Explicação</h2>
-      <p>Na área de bloqueio, basta digitar o nome do usuário (a famosa @) e em seguida, clicar/tocar no botão Enviar. Isso fará com que os tweets desse usuário sejam <strong>IMPEDIDOS</strong> de retweet pelo sistema.</p>
-      <p>Já na área de desbloqueio, o processo é bem semelhante: Digitar a @ no campo e depois, clicar ou tocar em enviar. Isso fará com que o bot seja novamente <strong>AUTORIZADO</strong> a dar RT nessa conta (e para ter o RT, a conta não pode estar trancada).</p>
-      <p>Lembre-se que o uso desse recurso está descrito nos termos de uso e, ao usá-lo, você está concordando com ele.</p>
+        <h2>Explicação</h2>
+        <p>Na área de bloqueio, basta digitar o nome do usuário (a famosa @) e em seguida, clicar/tocar no botão Enviar. Isso fará com que os tweets desse usuário sejam <strong>IMPEDIDOS</strong> de retweet pelo sistema.</p>
+        <p>Já na área de desbloqueio, o processo é bem semelhante: Digitar a @ no campo e depois, clicar ou tocar em enviar. Isso fará com que o bot seja novamente <strong>AUTORIZADO</strong> a dar RT nessa conta (e para ter o RT, a conta não pode estar trancada).</p>
+        <p>Lembre-se que o uso desse recurso está descrito nos termos de uso e, ao usá-lo, você está concordando com ele.</p>
 
-      <hr />
-      <h3>Bloqueio</h3>
-      <p>Esta é a parte para efetuar o <mark>bloqueio</mark> do bot na sua conta. Assim, ao perceber que um tweet pertence à você, ele é automaticamente ignorado.</p>
-      <form onSubmit={e => handleBlock(e)} className="row g-3">
-        <div className="input-group mb-3 col-auto">
-          <span className="input-group-text" id="user_block">@</span>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Usuário do Twitter"
-            aria-label="Usuário do Twitter para bloquear"
-            aria-describedby="user_block"
-            onChange={e => handleUser(e, true)}
-            value={userBlock}
-          />
-          <button className="btn btn-danger" type="submit">Bloquear</button>
+        <hr />
+        <h3>Bloqueio</h3>
+        <p>Esta é a parte para efetuar o <mark>bloqueio</mark> do bot na sua conta. Assim, ao perceber que um tweet pertence à você, ele é automaticamente ignorado.</p>
+        <form onSubmit={e => handleBlock(e)} className="row g-3">
+          <div className="input-group mb-3 col-auto">
+            <span className="input-group-text" id="user_block">@</span>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Usuário do Twitter"
+              aria-label="Usuário do Twitter para bloquear"
+              onChange={e => handleUser(e, true)}
+              value={userBlock}
+            />
+            <button className="btn btn-danger" type="submit">Bloquear</button>
+          </div>
+        </form>
+
+        <hr />
+        <h3>Desbloqueio</h3>
+        <p>Esta é a parte para voltar a <mark>autorizar</mark> o bot a dar RT na sua conta. Tweets feitos entre o bloqueio e o desbloqueio são ignorados.</p>
+        <form onSubmit={e => handleUnblock(e)} className="row g-3">
+          <div className="input-group mb-3 col-auto">
+            <span className="input-group-text" id="user_unblock">@</span>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Usuário do Twitter"
+              aria-label="Usuário do Twitter para desbloquear"
+              onChange={e => handleUser(e, false)}
+              value={userUnblock}
+            />
+            <button className="btn btn-primary" type="submit">Desbloquear</button>
+          </div>
+        </form>
+        <div className="position-relative" style={{ zIndex: 2000 }}>
+          <div className="toast-container position-absolute bottom-0 end-0 pb-3">
+            <div
+              className={`toast bg-${apiResponse.className}`}
+              role="alert"
+              aria-live="assertive"
+              aria-atomic="true"
+              id="liveToast"
+              data-bs-delay="10000"
+            >
+              <div className="toast-header">
+                <strong className="me-auto">{apiResponse.title}</strong>
+                <small>{apiResponse.time}</small>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="toast"
+                  aria-label="Fechar"
+                ></button>
+              </div>
+              <div className="toast-body">{apiResponse.message}</div>
+            </div>
+          </div>
         </div>
-      </form>
 
-      <hr />
-      <h3>Desbloqueio</h3>
-      <p>Esta é a parte para voltar a <mark>autorizar</mark> o bot a dar RT na sua conta. Tweets feitos entre o bloqueio e o desbloqueio são ignorados.</p>
-      <form onSubmit={e => handleUnblock(e)} className="row g-3">
-        <div className="input-group mb-3 col-auto">
-          <span className="input-group-text" id="user_unblock">@</span>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Usuário do Twitter"
-            aria-label="Usuário do Twitter para desbloquear"
-            aria-describedby="user_unblock"
-            onChange={e => handleUser(e, false)}
-            value={userUnblock}
-          />
-          <button className="btn btn-primary" type="submit">Desbloquear</button>
-        </div>
-      </form>
-    </div>
-  </main>
+      </div>
+    </main>
   );
 }
 
