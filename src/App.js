a3f96a23f4fb82f4bad747b79dcc106e30b23960
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-11 17:41:21 -0300

    Include notification to online and offline states

--- src/App.js
+++ src/App.js
@@ -10,22 +10,50 @@ const Routes = lazy(() => import('./routes'));
 const Footer = lazy(() => import('./components/Footer'));
 
 function App() {
-  const [show, setShow] = useState(false);
+  const [notification, setNotification] = useState(false);
+  const [info, setInfo] = useState({
+    title: '',
+    message: '',
+    className: ''
+  });
 
   useEffect(() => {
     window.addEventListener('updatefound', () => {
-      setShow(true);
+      setInfo({
+        title: 'Atualização',
+        message: 'Há uma atualização pendente para o app. Para concluir, basta fechar todas as abas do app e entrar novamente (sem reabrir a aba anterior).',
+        className: 'primary'
+      });
+      setNotification(true);
+    });
+
+    window.addEventListener('online', () => {
+      setInfo({
+          title: 'Você está online',
+          message: 'Sua internet está funcionando novamente e voltamos a ficar conectados com o servidor 🔛😎',
+          className: 'success'
+      });
+      setNotification(true);
+    });
+
+    window.addEventListener('offline', () => {
+      setInfo({
+          title: 'Você está offline',
+          message: 'Parece que há um problema na sua conexão e estamos offline📴😭. Verifique sua conexão antes de tentar realizar alguma ação online.',
+          className: 'warning text-dark'
+      });
+      setNotification(true);
     });
   }, []);
 
   return (
     <Suspense fallback={<Loading content="Preparando menu..." />} >
-      {show && (
-        <NotificationToast autoHide="false" style={{ zIndex: 3000 }} data={{
-          title: 'Atualização',
-          message: 'Há uma atualização pendente para o app. Para concluir, basta fechar todas as abas do app e entrar novamente (sem reabrir a aba anterior).',
-          className: 'primary'
-        }} systemCall />
+      {notification && (
+        <NotificationToast autoHide="false" data={info}>
+          <div className="d-grid gap-2 d-md-block pt-1" style={{ zIndex: 3100 }}>
+            <button className="btn btn-dark btn-sm" type="button" data-bs-dismiss="toast" onClick={e => setNotification(false)}>Fechar</button>
+          </div>
+        </NotificationToast>
       )}
       <Routes />
       <Footer />
