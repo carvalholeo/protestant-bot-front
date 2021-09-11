deaaefeea75ffa1eab927361fb1e8a7a23fe335f
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-11 19:03:18 -0300

    Remove PurgeCSS from CRACO config Add config to Sass files

--- craco.config.js
+++ craco.config.js
@@ -1,21 +1,30 @@
-const path = require("path");
-const fs = require("fs");
-const glob = require("glob-all");
-
-const appDirectory = fs.realpathSync(process.cwd());
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
-
-const PurgecssPlugin = require("purgecss-webpack-plugin");
-
 module.exports = {
   webpack: {
-    plugins: [
-      new PurgecssPlugin({
-        paths: [
-          resolveApp("public/index.html"),
-          ...glob.sync(`${resolveApp("src")}/**/**/*`, { nodir: true })
-        ]
-      })
-    ]
+    test: /\.(scss)$/,
+    use: [{
+      // inject CSS to page
+      loader: 'style-loader'
+    }, {
+      // translates CSS into CommonJS modules
+      loader: 'css-loader'
+    }, {
+      // Run postcss actions
+      loader: 'postcss-loader',
+      options: {
+        // `postcssOptions` is needed for postcss 8.x;
+        // if you use postcss 7.x skip the key
+        postcssOptions: {
+          // postcss plugins, can be exported to postcss.config.js
+          plugins: function () {
+            return [
+              require('autoprefixer')
+            ];
+          }
+        }
+      }
+    }, {
+      // compiles Sass to CSS
+      loader: 'sass-loader'
+    }]
   }
 };
