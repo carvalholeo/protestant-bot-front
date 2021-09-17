575f316c2f0e09f725ce12d99c675b50bda527a4
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-17 00:46:01 -0300

    Create a Wrapper to Helmet component

---
+++ src/components/HelmetWrapper/index.js
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet";
+
+function HelmetWrapper({ title, canonical }) {
+  return (
+    <Helmet titleTemplate="%s | Protestant Bot" defaultTitle="Gerenciamento do BotLutero">
+      <title>{title}</title>
+      {canonical && (
+        <link rel="canonical" href={`${process.env.PUBLIC_URL}/${canonical}`} />
+      )}
+    </Helmet>
+  );
+}
+
+export default HelmetWrapper;
