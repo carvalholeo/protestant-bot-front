ec7c5f88a4c09ca60e56f43650a14c90321f0d53
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-18 00:23:14 -0300

    Include children to Helmet component

--- src/components/HelmetWrapper/index.js
+++ src/components/HelmetWrapper/index.js
@@ -1,9 +1,11 @@
 import { Helmet } from "react-helmet-async";
 
-function HelmetWrapper({ title, canonical }) {
+function HelmetWrapper({ title, canonical, keywords, children }) {
   return (
     <Helmet titleTemplate="%s | Protestant Bot" defaultTitle="Gerenciamento do BotLutero">
       <title>{title}</title>
+      <meta name="keywords" content={`bot lutero, martinho lutero, retweetar reforma protestante, reforma protestante, lutero bot, dia da reforma, dia do protesto, ${keywords}`} />
+      {children}
       {canonical && (
         <link rel="canonical" href={`${process.env.PUBLIC_URL}/${canonical}`} />
       )}
