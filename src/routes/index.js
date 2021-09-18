a144999d6bec65d8dd873312e6d2174de6f11fe6
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-18 20:47:41 -0300

    Implement Location Context into routes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Switch } from "react-router-dom";
+import { Switch, useLocation } from "react-router-dom";
 
 import Loading from '../components/Loading';
+import { LocationContext } from "../contexts/LocationContext";
 
 import Route from './Route';
 
@@ -14,19 +15,26 @@ const Contact = React.lazy(() => import("../pages/Contact"));
 const Error404 = React.lazy(() => import("../pages/Errors/404"));
 
 function Routes() {
-  return (
-      <React.Suspense fallback={<Loading content="Abrindo conteúdo..." />} >
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={About} />
-          <Route path="/releasenotes" component={ReleaseNotes} />
-          <Route path="/privacy" component={PrivacyPolicy} />
-          <Route path="/block-manager" component={BlockManager} />
-          <Route path="/contact" component={Contact} />
+  const location = useLocation();
+  const { changePathName } = React.useContext(LocationContext);
 
-          <Route component={Error404} />
-        </Switch>
-      </React.Suspense>
+  React.useEffect(() => {
+    changePathName(location)
+  }, [location, changePathName]);
+
+  return (
+    <React.Suspense fallback={<Loading content="Abrindo conteúdo..." />} >
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={About} />
+        <Route path="/releasenotes" component={ReleaseNotes} />
+        <Route path="/privacy" component={PrivacyPolicy} />
+        <Route path="/block-manager" component={BlockManager} />
+        <Route path="/contact" component={Contact} />
+
+        <Route component={Error404} />
+      </Switch>
+    </React.Suspense>
   );
 }
 
