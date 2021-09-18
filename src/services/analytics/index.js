fe144e9b2e3b78334db0a797b8e2db16e7bbce70
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-18 20:46:20 -0300

    Remove useAckee hook Refactor options to a const Create function to handle with web vitals metrics

--- src/services/analytics/index.js
+++ src/services/analytics/index.js
@@ -1,17 +1,42 @@
-import useAckee from "use-ackee";
+import { create } from 'ackee-tracker';
 
 const SERVER = process.env.REACT_APP_ANALYTICS_SERVER;
 const DOMAIN_ID = process.env.REACT_APP_ANALYTICS_ID;
+const EVENT_ID = process.env.REACT_APP_EVENT_ID;
+
+const OPTIONS = {
+  detailed: true,
+  ignoreLocalhost: true,
+  ignoreOwnVisits: true
+}
+
+const WEB_VITALS = {
+  CLS: 'Cumulative Layout Shift',
+  LCP: 'Largest Contentful Paint',
+  TTFB: 'Time to Transfer First Byte',
+  FCP: 'First Contentful Paint',
+  FID: 'First Input Delay',
+  TTI: 'Time to Interactive',
+  TBT: 'Total Blocking Time'
+};
 
 function Analytics(pathname) {
-  useAckee(pathname, {
-    domainId: DOMAIN_ID,
-    server: SERVER
-  }, {
-    detailed: true,
-    ignoreLocalhost: true,
-    ignoreOwnVisits: true
+  const tracker = create(SERVER, OPTIONS);
+  tracker.record(DOMAIN_ID, {
+    siteLocation: pathname,
+    siteReferrer: document.referrer,
   });
 }
+
+function analyticsActions({ name, value }) {
+  const action = WEB_VITALS[name];
+  const tracker = create(SERVER, OPTIONS);
+  tracker.action(EVENT_ID, {
+    key: action,
+    value: value
+  });
+}
+
+export { analyticsActions };
 
 export default Analytics;
