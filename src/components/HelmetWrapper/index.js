cc0ebe20db254163bc269ccfe4415e771c04545f
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-18 01:18:30 -0300

    Include description tag and property to Helmet

--- src/components/HelmetWrapper/index.js
+++ src/components/HelmetWrapper/index.js
@@ -1,14 +1,17 @@
 import { Helmet } from "react-helmet-async";
 
-function HelmetWrapper({ title, canonical, keywords, children }) {
+function HelmetWrapper({ title, canonical, keywords, description, children }) {
   return (
     <Helmet titleTemplate="%s | Protestant Bot" defaultTitle="Gerenciamento do BotLutero">
       <title>{title}</title>
       <meta name="keywords" content={`bot lutero, martinho lutero, retweetar reforma protestante, reforma protestante, lutero bot, dia da reforma, dia do protesto, ${keywords}`} />
-      {children}
+      {description && (
+        <meta name="description" content={description} />
+      )}
       {canonical && (
         <link rel="canonical" href={`${process.env.PUBLIC_URL}/${canonical}`} />
       )}
+      {children}
     </Helmet>
   );
 }
