64c166a68c903fbbd0899165c5be634bf3587b4b
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-20 20:31:07 -0300

    Remove repeated config

--- craco.config.js
+++ craco.config.js
@@ -2,33 +2,6 @@ const sitemapGenerator = require('./src/services/sitemap-generator');
 
 module.exports = {
   webpack: {
-    test: /\.(scss)$/,
-    use: [{
-      // inject CSS to page
-      loader: 'style-loader'
-    }, {
-      // translates CSS into CommonJS modules
-      loader: 'css-loader'
-    }, {
-      // Run postcss actions
-      loader: 'postcss-loader',
-      options: {
-        // `postcssOptions` is needed for postcss 8.x;
-        // if you use postcss 7.x skip the key
-        postcssOptions: {
-          // postcss plugins, can be exported to postcss.config.js
-          plugins: function () {
-            return [
-              require('autoprefixer')
-            ];
-          }
-        }
-      }
-    }, {
-      // compiles Sass to CSS
-      loader: 'sass-loader'
-    }],
-    externals: ["react-helmet"],
     plugins: [
       {
         apply: (compiler) => {
