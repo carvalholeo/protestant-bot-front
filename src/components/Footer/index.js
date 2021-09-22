4bd7acbaa76b5a6a8d524fb9209410c6caeb2f20
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-22 01:43:01 -0300

    Fix bug at Footer positioning

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,16 +6,16 @@ const Twemoji = lazy(() => import("react-twemoji"));
 function Footer() {
   return (
     <Suspense fallback={<Loading content="Montando página..." />}>
-      <Twemoji options={{ className: "emoji" }}>
-        <footer className="footer mt-auto py-3 bg-dark" id="footer">
+      <footer className="footer mt-auto py-3 bg-dark" id="footer">
+        <Twemoji options={{ className: "emoji" }}>
           <section className="container text-center">
             <small className="text-light">
               Feito com <span className="emoji">♥</span> por Léo Carvalho no
               Guarujá/SP. <span className="emoji">🇧🇷</span>
             </small>
           </section>
-        </footer>
-      </Twemoji>
+        </Twemoji>
+      </footer>
     </Suspense>
   );
 }
