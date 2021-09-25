0b0061647578c874a16a8696e8805e04ff6a2ddb
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-24 23:28:57 -0300

    Implement rule to watch localStorage

--- src/contexts/DarkModeContext.js
+++ src/contexts/DarkModeContext.js
@@ -1,18 +1,40 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 export const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
   const [isDark, setIsDark] = useState(true);
+  const [theme, setTheme] = useState(window.localStorage.getItem('color-theme'));
+
+  useEffect(() => {
+    setTheme(window.localStorage.getItem('color-theme'));
+  }, []);
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      setIsDark(true);
+      return;
+    }
+    setIsDark(false);
+  }, [theme]);
+
+  useEffect(() => {
+    if (isDark) {
+      window.localStorage.setItem('color-theme', 'dark');
+      return;
+    }
+    window.localStorage.setItem('color-theme', 'light');
+  }, [isDark]);
 
   function changeDarkMode(dark) {
     setIsDark(dark);
   }
+
   return (
     <DarkModeContext.Provider value={{ isDark, changeDarkMode }}>
       {children}
     </DarkModeContext.Provider>
-  )
+  );
 }
 
 export default DarkModeProvider;
